f2f1490ab584255c2e5a3df3ff5c22d7ec1a8962
BNgọc Sơn <bns050404@gmail.com>
2025-11-11 09:33:35 +0700

    update cors

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ultimatemcqsagent.vercel.app"],
+    allow_origins=["https://bnson.id.vn"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
